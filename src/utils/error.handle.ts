6faaa51dc1de5e8d8db6e7653937686d29b36fa4
Briamco <crossinerobriamco@gmail.com>
2025-04-27 19:16:08 -0400

    fix: set default status code to 500 in error handling function

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { Error } from "../interfaces/error";
 
-const handleHTTP = (res: Response, error: string, status: number) => {
+const handleHTTP = (res: Response, error: string, status: number = 500) => {
   console.error(error)
   res.status(status)
   res.send({ error })
