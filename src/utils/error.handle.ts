04d338a23f5f76f7aaa7b494240ce85639c34129
Briamco <crossinerobriamco@gmail.com>
2025-04-27 21:37:47 -0400

    fix: remove console error logging from HTTP error handler

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,8 +1,6 @@
 import { Response } from "express";
-import { Error } from "../interfaces/error";
 
 const handleHTTP = (res: Response, error: string, status: number = 500) => {
-  console.error(error)
   res.status(status)
   res.send({ error })
 }
