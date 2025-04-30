cf55c213188a87dd63b69e7fd4c8aa271af44233
Briamco <crossinerobriamco@gmail.com>
2025-04-30 11:26:39 -0400

    fix(cors): configure CORS to allow specific origin and enable credentials

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,10 @@ import { router, authRouter } from './routes'
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--d4eba4a9.local-credentialless.webcontainer-api.io', // O usa una función si quieres múltiples orígenes
+  credentials: true               // Esto es lo que permite enviar cookies
+}))
 app.use('/api', router)
 app.use('/auth', authRouter)
 
